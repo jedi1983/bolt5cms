262e9b9622e5fe1742962e47e2874aeaafabf783
Roland Antonius <jedi1983@Rolands-MacBook-Air.local>
2021-12-15 16:59:52 +0700

    first commit

---
+++ config/bundles.php
@@ -0,0 +1,81 @@
+<?php
+
+return [
+    'ApiPlatform\\Core\\Bridge\\Symfony\\Bundle\\ApiPlatformBundle' => [
+        'all' => true,
+    ],
+    'BabDev\\PagerfantaBundle\\BabDevPagerfantaBundle' => [
+        'all' => true,
+    ],
+    'DAMA\\DoctrineTestBundle\\DAMADoctrineTestBundle' => [
+        'test' => true,
+    ],
+    'Doctrine\\Bundle\\DoctrineBundle\\DoctrineBundle' => [
+        'all' => true,
+    ],
+    'Doctrine\\Bundle\\FixturesBundle\\DoctrineFixturesBundle' => [
+        'dev' => true,
+        'test' => true,
+        'local' => true,
+    ],
+    'Knp\\Bundle\\MenuBundle\\KnpMenuBundle' => [
+        'all' => true,
+    ],
+    'Knp\\DoctrineBehaviors\\DoctrineBehaviorsBundle' => [
+        'all' => true,
+    ],
+    'Nelmio\\CorsBundle\\NelmioCorsBundle' => [
+        'all' => true,
+    ],
+    'Nelmio\\SecurityBundle\\NelmioSecurityBundle' => [
+        'all' => true,
+    ],
+    'Sensio\\Bundle\\FrameworkExtraBundle\\SensioFrameworkExtraBundle' => [
+        'all' => true,
+    ],
+    'Symfony\\Bundle\\DebugBundle\\DebugBundle' => [
+        'dev' => true,
+        'test' => true,
+        'local' => true,
+    ],
+    'Symfony\\Bundle\\FrameworkBundle\\FrameworkBundle' => [
+        'all' => true,
+    ],
+    'Symfony\\Bundle\\MonologBundle\\MonologBundle' => [
+        'all' => true,
+    ],
+    'Symfony\\Bundle\\SecurityBundle\\SecurityBundle' => [
+        'all' => true,
+    ],
+    'Symfony\\Bundle\\TwigBundle\\TwigBundle' => [
+        'all' => true,
+    ],
+    'Symfony\\Bundle\\WebProfilerBundle\\WebProfilerBundle' => [
+        'dev' => true,
+        'local' => true,
+    ],
+    'Symfony\\Bundle\\WebServerBundle\\WebServerBundle' => [
+        'dev' => true,
+        'test' => true,
+        'local' => true,
+    ],
+    'Symfony\\WebpackEncoreBundle\\WebpackEncoreBundle' => [
+        'all' => true,
+    ],
+    'Symplify\\ConsoleColorDiff\\ConsoleColorDiffBundle' => [
+        'dev' => true,
+        'test' => true,
+    ],
+    'Translation\\Bundle\\TranslationBundle' => [
+        'all' => true,
+    ],
+    'Twig\\Extra\\TwigExtraBundle\\TwigExtraBundle' => [
+        'all' => true,
+    ],
+    'SymfonyCasts\\Bundle\\ResetPassword\\SymfonyCastsResetPasswordBundle' => [
+        'all' => true,
+    ],
+    'Doctrine\\Bundle\\MigrationsBundle\\DoctrineMigrationsBundle' => [
+        'all' => true,
+    ],
+];
